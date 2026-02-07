13ea31a4c41f3a7d47447fc2e8c01618e8500f31
Niels Hofma <nielsh83@gmail.com>
2026-02-07 15:46:41 +0100

    Remove .vercel from repo and let Vercel build normally

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_C7AmF7V5.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_D4L7SFv0.mjs';
-export { renderers } from '../renderers.mjs';
